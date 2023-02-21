91be76308595254b80806c892a25d52e3dce8e84
julien.spenst <julienspenst06@gmail.com>
2023-02-21 09:47:59 +0100

    .

--- src/Ufo.java
+++ src/Ufo.java
@@ -1,42 +1,24 @@
 import GLOOP.*;
 public class Ufo{
-    private GLTorus rumpf; 
-    private GLKugel cockpit;
-    private GLKegel fluegel1, fluegel2;
-    //[...]
+    GLKegel k1;
 
     public Ufo(){
-        rumpf = new GLTorus(0,0,0,50,20);
-        rumpf.skaliere (0.5,1,0.5);
-        rumpf.setzeQualitaet(24);
-
-        cockpit = new GLKugel(0,0,0,25);
-        cockpit.setzeMaterial(GLMaterial.GOLD);
-
-        fluegel1 = new GLKegel(-60,-10,0,15,70);
-        fluegel1.skaliere(0.2,1,1);        
-        fluegel1.drehe(0,90,0, -60,10,0);
-        fluegel1.drehe(0,0,45, 0,0,0);
-        fluegel1.setzeFarbe(0,0,1);
-
-        fluegel2 = new GLKegel(60,-10,0,15,70);
-        fluegel2.skaliere(0.2,1,1);        
-        fluegel2.drehe(0,-90,0, 60,10,0);
-        fluegel2.drehe(0,0,-45, 0,0,0);
-        fluegel2.setzeFarbe(0,0,1);  
-        //[...]
+        k1 = new GLKegel(0,0,0,5,10);
+        k1.drehe(90,0,0);
+        k1.setzeFarbe(136,0,255);
     }
- 
     public void bewegeLinks(){
-        //[...]
+        k1.verschiebe(-1,0,0);
     }
-
-    public void bewegeRechts(){       
-        //[...]       
-    } 
-
+    public void bewegeRechts(){
+        k1.verschiebe(1,0,0);
+    }
+    public void bewegeoben(){
+        k1.verschiebe(0,1,0);
+    }
+    public void bewegeunten(){
+        k1.verschiebe(0,-1,0);
+    }
     public void explodiere(){
-        //[...]
     }
-
 }
--- src/Ufospiel.java
+++ src/Ufospiel.java
@@ -1,36 +1,36 @@
 import GLOOP.*; 
-public class Ufospiel{
+public class Ufospiel {
     private GLKamera kamera;
     private GLLicht licht;
     private GLTastatur tastatur;
     private GLHimmel himmel;
+    private Ufo meinUfo;
+    public Ufo k1;
 
-    private Ufo dasUfo;
-    private Asteroid asteroid1,asteroid2,asteroid3;
-    //[...]
-
-    public Ufospiel(){
-        kamera = new GLSchwenkkamera(800,600);  
-        kamera.verschiebe(0,200,500);
-        licht  = new GLLicht();
+    public Ufospiel() {
+        kamera = new GLEntwicklerkamera(800, 600);
+        kamera.verschiebe(0, 200, 500);
+        licht = new GLLicht();
         tastatur = new GLTastatur();
         himmel = new GLHimmel("src/img/Sterne.jpg");
-
-        dasUfo = new Ufo();
-
-        asteroid1 = new Asteroid();
-        //[...]
-        
-        fuehreAus();
+        k1 = new Ufo();
+        run();
     }
-
-    public void fuehreAus(){
-        while(!tastatur.esc()){           
-            asteroid1.bewegeDich();
-            //[...]
-
+    public void run(){
+        while(!tastatur.esc()){
+            if(tastatur.rechts()){
+                k1.bewegeRechts();
+            }
+            if(tastatur.links()){
+                k1.bewegeLinks();
+            }
+            if(tastatur.oben()){
+                k1.bewegeoben();
+            }
+            if(tastatur.unten()){
+                k1.bewegeunten();
+            }
             Sys.warte();
         }
-        Sys.beenden(); 
     }
 }
