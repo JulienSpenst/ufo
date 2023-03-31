f766a081694344af4dd462ffbe400cc3e9694d5e
julien.spenst <julienspenst06@gmail.com>
2023-03-31 09:44:26 +0200

    .

--- src/Asteroid.java
+++ src/Asteroid.java
@@ -6,17 +6,38 @@ public class Asteroid{
     double pY;
     Ufo dasUfo;
     public Asteroid(Ufo pUfo){
-        kug1 = new GLKugel(Math.random()*1400 - 700,Math.random()*1000 + 700,0,50);
+        kug1 = new GLKugel(Math.random()*1400 - 700,Math.random()*1000 + 700,0, 50);
+        kug1.setzeFarbe(1,0,0);
         dasUfo = pUfo;
     }
     public void astebewege(){kug1.verschiebe(0,-1,0);
         pY = kug1.gibY();
         if (pY < -350) {
         kug1.setzePosition(Math.random()*1400 - 700, Math.random()*1000 + 700, 0);
+        if(getroffen()){
+            dasUfo.explodiere();
+        }
     }
     }
 
     public double gibX(){
         return kug1.gibX();
     }
+    public double gibY(){
+        return kug1.gibY();
+    }
+    public double gibZ(){
+        return kug1.gibZ();
+    }
+    public boolean getroffen(){
+       double abstand = Math.sqrt(
+               Math.pow(kug1.gibX()-dasUfo.gibX(),2)+ Math.pow(kug1.gibX()-dasUfo.gibX(),2)+ Math.pow(kug1.gibX()-dasUfo.gibX(),2));
+       if(abstand < 50){
+           return true;
+       }
+       else{
+           return false;
+       }
+    }
 }
+
--- src/Ufo.java
+++ src/Ufo.java
@@ -3,20 +3,33 @@ public class Ufo{
     GLKegel k1;
 
     public Ufo(){
-        k1 = new GLKegel(0,0,0,5,10);
+        k1 = new GLKegel(0,0,0,7,12);
         k1.drehe(90,0,0);
-        k1.setzeFarbe(136,0,255);
+        k1.setzeFarbe(0,1,0);
     }
-    public void bewegeLinks(){k1.verschiebe(-2,0,0);}
+    public void bewegeLinks(){k1.verschiebe(-1.5,0,0);}
     public void bewegeRechts(){
-        k1.verschiebe(2,0,0);
+        k1.verschiebe(1.5,0,0);
     }
     public void bewegeoben(){
-        k1.verschiebe(0,2,0);
+        k1.verschiebe(0,1.5,0);
     }
     public void bewegeunten(){
-        k1.verschiebe(0,-2,0);
+        k1.verschiebe(0,-1.5,0);
     }
     public void explodiere(){
+        double z = Math.random();
+        for(int i=0;i<2000;i++){
+          Sys.warte();
+        }
+    }
+    public double gibX(){
+        return k1.gibX();
+    }
+    public double gibY(){
+        return k1.gibY();
+    }
+    public double gibZ(){
+        return k1.gibZ();
     }
 }
